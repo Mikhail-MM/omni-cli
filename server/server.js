1f5ab70d74e6d70b3e27a240eb31857db9b25b64
Mikhail-MM <leakymee@bu.edu>
2020-01-07 00:03:10 -0800

    Remove useless code

--- server/server.js
+++ server/server.js
@@ -9,12 +9,6 @@ import getEnv from '../lib/utils/get-env';
 import { establishMongooseConnection } from '../lib/mongo/mongoose-db';
 import { registerMongooseUserPathways } from './router/users';
 
-/*
-  import router from './router/index';
-  console.log(util.inspect(router, false, null, true));
-  const { Router1, Router2 } = router;
-*/
-
 getEnv()
 
 const app = express();
@@ -43,7 +37,6 @@ if (app.get('env') === 'development') {
   });
 }
 
-console.log(process.env.NODE_ENV);
 establishMongooseConnection()
   .then(connection => {
     if (connection.success) {
@@ -54,7 +47,6 @@ establishMongooseConnection()
     console.log(err);
   })
 
-// app.use('/REST-TEST', RESTRouter, messagesRouter);
 app.get('/', (req, res) => {
   res.send('Hello!')
 });
@@ -63,10 +55,10 @@ app.get('/api', (req, res) => {
   res.send('NginX proxy should forward around this, PERHAPS?')
 });
 
+// Register new users and store in MongoDB
 registerMongooseUserPathways(app);
 
 // Universal Error Handler
-
 app.use('/', (err, req, res, next) => {
   handleError(err);
   res.status(err.status || 500).json({ error: err.stack, message: err.message });
