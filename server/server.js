1a4af2f7379542e57b209f8e5ec2cdca43a40a37
Mikhail-MM <leakymee@bu.edu>
2020-01-07 02:15:58 -0800

    Added cookie-based auth  route scaffold

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 import util from 'util';
 
 import express from 'express';
-// import cookieParser from 'cookie-parser'
+import cookieParser from 'cookie-parser'
 import morgan from 'morgan';
 import { handleError } from '../lib/utils/logger';
 import getEnv from '../lib/utils/get-env';
@@ -16,7 +16,7 @@ const app = express();
 app.use(morgan('common'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-// app.use(cookieParser())
+app.use(cookieParser())
 
 if (app.get('env') === 'development') {
   console.log('Configuring Access Control Allow Origin header for Local Development.');
@@ -27,7 +27,7 @@ if (app.get('env') === 'development') {
     );
     res.header(
       'Access-Control-Allow-Headers',
-      'Origin, X-Requested-With, Content-Type, Accept, Authorization',
+      'Origin, X-Requested-With, Content-Type, Accept, Authorization, Set-Cookie',
     )
     res.header(
       'Access-Control-Allow-Methods',
@@ -60,13 +60,19 @@ app.use('/', (req, res, next) => {
   // Cookie Middleware  
   // Send back secure HTTPONLY cookie on successful auth
   // Can we send secure on local dev?
-  const authTimeOut = 3 * 60 * 60 * 1000 // 3 hrs
+  const maxAge = 3 * 60 * 60 * 1000 // 3 hrs
   console.log("Cookies: ");
   console.log(req.cookies);
   res.header(
     'Set-Cookie',
-    `Stuff=HI!; Max-Age=${authTimeOut};`
+    `Stuff=HI!; Max-Age=${maxAge};`
   );
+  res.cookie('Authorization-Omni', 'sec0ret encu0ingdgin', {
+    maxAge,
+    httpOnly: true,
+    secure: true,
+  });
+
   next();
 })
 
